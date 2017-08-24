53164a640a02d89162192588de93bc02402da5f2
Shinasaki <Shinasaki07@gmail.com>
2017-08-25 00:15:51 +0700

    add Structure

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 <html>
     <head>
 
+        
+        <!-- ดึงไฟล์ภายนอก -->
+        <link href="https://fonts.googleapis.com/css?family=Itim" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 
-        <!-- ดึงไฟล์ -->
+
+        <!-- ดึงไฟล์ภายใน -->
         <?php require_once 'autoload/init.php' ?>
 
 
@@ -12,15 +17,25 @@
 
         <!-- ชื่อเว็ปแก้ไขได้ใน config/baseName.php -->
         <title><?php echo baseName; ?></title>
+
+
     </head>
     <body>
+
+
         <!-- ดึงส่วน Header -->
         <?php require_once 'views/layout/header.php' ?>
 
+
         <!-- ดึงส่วน Content -->
+        <div class="content">
         <?php require_once 'autoload/route.php' ?>
+        </div>
+
 
         <!-- ดึงส่วน Footer -->
         <?php require_once 'views/layout/footer.php' ?>
+
+
     </body>
 </html>
--- views/layout/header.php
+++ views/layout/header.php
@@ -1,9 +1,11 @@
 <div class="header">
     <ul>
-        <a href=""><li>Home</li></a>
+        <a href="?"><li>Blog <i class="fa fa-rocket" aria-hidden="true"></i> Space |</li></a>
+        <a href="?page=myblog"><li>My Blog </li></a>
+        <a href="?page=myblog"><li>Write Blog </li></a>
     </ul>
     <ul>
-        <a href=""><li>Sign In</li></a>
-        <a href=""><li>Sign Up</li></a>
+        <a href="?page=login"><li>Sign In</li></a>
+        <a href="?page=register"><li>Sign Up</li></a>
     </ul>
 </div>
